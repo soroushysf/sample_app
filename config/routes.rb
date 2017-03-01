39d0fcc2f26f16fafc39fd6608d93cc7672f970b
soroush yousefi <soroush.ysf@gmail.com>
2017-03-02 00:49:36 +0330

    layout finished and bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
     
-  get 'static_pages/home'
+    get '/home', to: 'static_pages#home'
 
-  get 'static_pages/help'
+    get '/help', to: 'static_pages#help'
     
-  get 'static_pages/about'
+    get '/about', to: 'static_pages#about'
     
-  get 'static_pages/contact'
+    get '/contact', to: 'static_pages#contact'
   
-  get 'static_pages/palindrome'
+    get '/palindrome', to: 'static_pages#palindrome'
   
-
+    get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     

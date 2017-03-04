01f48612ed695bdd5a55ef0a70109f20e550782f
soroush yousefi <soroush.ysf@gmail.com>
2017-03-04 17:04:56 +0330

    form made for creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+      @user = User.new(name: "example", email: "example@example.com")
   end
+    
+    def create 
+        @user = User.new(user_params)
+        
+        if @user.save
+            
+            flash[:notice] = "User '#{@user.name}' created successfully !"
+            redirect_to(home_path)
+        else
+            flash[:error] = "User not created successfully, please follow as the form sais !"
+            redirect_to(signup_path)
+        end
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     get '/palindrome', to: 'static_pages#palindrome'
   
     get '/signup', to: 'users#new'
+    
+    post '/signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     

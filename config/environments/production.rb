ba1f6f3951bb88b387e39decc191afaa5d088ecc
soroush yousefi <soroush.ysf@gmail.com>
2017-04-10 19:21:47 +0430

    production email change v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
       domain: "https://still-everglades-44161.herokuapp.com",
